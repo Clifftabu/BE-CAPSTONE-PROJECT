3571f79da1254fb582d3dd6bb991b38c3a15388a
CLIFFTABU <clifftabu@gmail.com>
2025-05-09 22:51:37 +0300

    Update project with latest changes

--- task_management/urls.py
+++ task_management/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet
+from tasks.views import TaskViewSet
+
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,9 @@
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework import serializers
+
+
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
